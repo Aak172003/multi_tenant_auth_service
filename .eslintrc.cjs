197325241deb6c1b99f734f87b7cb0349e04d1ae
Aak172003 <prajapatiaakash364@gmail.com>
2024-09-12 08:29:45 +0530

    add eslint setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+/* eslint-env node */
+module.exports = {
+    extends: [
+        // This is recommended rules
+        "eslint:recommended",
+
+        // This is typescript rules which is recommended
+        // "plugin:@typescript-eslint/recommended",
+
+        "plugin:@typescript-eslint/recommended-type-checked",
+
+        // Linters and Prettiers
+        "prettier",
+    ],
+    parser: "@typescript-eslint/parser",
+    plugins: ["@typescript-eslint"],
+    // refer to tsconfig file , for types information
+    parserOptions: {
+        projectService: true,
+        tsconfigRootDir: __dirname,
+        // __dirname -> this means current folder
+    },
+    root: true,
+    rules: {
+        // if console give error
+        "no-console": "error",
+        "dot-notation": "error",
+    },
+};
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,20 @@
 // This function return type is string
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+// .
+// .
+
 function login(username: string): string {
     console.log("username ------ username ----- ", username);
+
+    const user = {
+        name: "Aakash",
+        age: 17,
+    };
+    // eslint fix into dot notation
+
+    const name = user.name;
+    console.log(name);
 
     return username;
 }
