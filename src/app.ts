7ec288a8e4471cad8217716bb4dc45471b48f6f8
Aakashmoondive502 <aakash.prajapati@moondive.co>
2024-09-14 00:13:36 +0530

    setup winston logger for logs where transport mode is file and console

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,6 @@ import express from "express";
 
 const app = express();
 
-// console.log("app=========", app);
-
 app.get("/", (req, res) => {
     res.json({
         message: "Welcome to Auth Service",
