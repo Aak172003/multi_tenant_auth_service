b8ba8452165c57395d5dc20ab2e8bb036f0bdefb
Aakashmoondive502 <aakash.prajapati@moondive.co>
2024-09-14 00:12:37 +0530

    setup winston logger for logs where transport mode is file and console

---
+++ src/config/logger.ts
@@ -0,0 +1,57 @@
+import winston from "winston";
+import { Config } from ".";
+
+const logger = winston.createLogger({
+    // This level is default,
+    // like when i forget to add level, so below tranport inherit this level inside
+    level: "info",
+    defaultMeta: {
+        serviceName: "auth-service",
+    },
+    transports: [
+        new winston.transports.File({
+            dirname: "logs",
+            filename: "combined.log",
+            // Because , silly is last level ,
+            // and above this level all logger level will show on combined logs
+            level: "silly",
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json(),
+            ),
+            // if silent true , then no console logs create
+
+            silent: Config.NODE_ENV === "test",
+            // Which means silent is true  in test mode
+        }),
+
+        new winston.transports.File({
+            dirname: "logs",
+            filename: "error.log",
+            level: "error",
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json(),
+            ),
+            // if silent true , then no console logs create
+            silent: Config.NODE_ENV === "test",
+            // Which means silent is true  in test mode
+        }),
+        new winston.transports.Console({
+            level: "info",
+            // format: winston.format.simple(),
+            // format: winston.format.json(),
+
+            // This combine keyword is used to apply more than 2 formats
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.json(),
+            ),
+            // if silent true , then no console logs create
+            silent: Config.NODE_ENV === "test",
+            // Which means silent is true  in test mode
+        }),
+    ],
+});
+
+export default logger;
--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,39 @@
 import app from "./app";
 import { Config } from "./config";
+import logger from "./config/logger";
 
 const startServer = () => {
     const PORT = Config.PORT;
     const ENVIRONMENT = Config.NODE_ENV;
     try {
+        // throw new Error("something went wrong");
         app.listen(PORT, () => {
-            console.log("---------------------------------------");
             console.log(
                 `Welcome to Port No.${PORT} , My Current Env is ${ENVIRONMENT}`,
             );
+
+            logger.info(`Listning on PORT : ${PORT}`, {
+                test: "Server Listen or not ",
+            });
+
+            logger.silly("This is silly logger");
+            logger.info("This is Info file ", {
+                test: "This is test for info file",
+            });
         });
-    } catch (error) {
-        console.log(error);
-        process.exit(1);
+    } catch (error: unknown) {
+        // Bydefault this error is unknown type error ,
+        // so first we need to verify is this error is an instance of Error ,
+        // then we will execute logger.error
+        // console.log(error);
+
+        if (error instanceof Error) {
+            logger.error(error.message, { test: " test error file " });
+
+            setTimeout(() => {
+                process.exit(1);
+            }, 1000);
+        }
     }
 };
 
